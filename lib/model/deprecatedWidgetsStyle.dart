8f2809a2973f85c742194e05096beca16d931b89
dev-raj-1729 <dev.raj.r1729@gmail.com>
2021-05-20 15:24:00 +0530

    added outlineButtonStyle

--- lib/model/deprecatedWidgetsStyle.dart
+++ lib/model/deprecatedWidgetsStyle.dart
@@ -20,3 +20,12 @@ final ButtonStyle raisedButtonStyle = ElevatedButton.styleFrom(
     borderRadius: BorderRadius.all(Radius.circular(2)),
   ),
 );
+
+final ButtonStyle outlineButtonStyle = OutlinedButton.styleFrom(
+  primary: Colors.black87,
+  minimumSize: Size(88, 36),
+  padding: EdgeInsets.symmetric(horizontal: 16),
+  shape: const RoundedRectangleBorder(
+    borderRadius: BorderRadius.all(Radius.circular(2)),
+  ),
+);
