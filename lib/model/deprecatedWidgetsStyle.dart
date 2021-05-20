4a3634beed00849560eb18578f004258e86e1496
dev-raj-1729 <dev.raj.r1729@gmail.com>
2021-05-20 15:07:41 +0530

    added raised ButtonStyle

--- lib/model/deprecatedWidgetsStyle.dart
+++ lib/model/deprecatedWidgetsStyle.dart
@@ -10,3 +10,13 @@ final ButtonStyle flatButtonStyle = TextButton.styleFrom(
     borderRadius: BorderRadius.all(Radius.circular(2.0)),
   ),
 );
+
+final ButtonStyle raisedButtonStyle = ElevatedButton.styleFrom(
+  onPrimary: Colors.black87,
+  primary: Colors.grey[300],
+  minimumSize: Size(88, 36),
+  padding: EdgeInsets.symmetric(horizontal: 16),
+  shape: const RoundedRectangleBorder(
+    borderRadius: BorderRadius.all(Radius.circular(2)),
+  ),
+);
